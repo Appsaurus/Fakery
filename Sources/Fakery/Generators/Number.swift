19637d073bceb2d2b7133d9e2c6f04a982fe5e8a
Brian Strobach <brian@appsaurus.io>
2019-05-01 17:37:36 -0500

    Fixed warning

--- Sources/Fakery/Generators/Number.swift
+++ Sources/Fakery/Generators/Number.swift
@@ -37,7 +37,7 @@ public final class Number {
 
 fileprivate extension Double {
 	/// Rounds the double to decimal places value
-	fileprivate func rounded(toPlaces places:Int) -> Double {
+    func rounded(toPlaces places:Int) -> Double {
 		let divisor = pow(10.0, Double(places))
 		return (self * divisor).rounded() / divisor
 	}
